06d8b687d381eb5f055ac9b33d8d3b1ecffd1c4b
marioavaj <predaj@predaj-tonerov.sk>
2022-05-18 10:43:13 +0200

    newsletter design

--- slideshow.js
+++ slideshow.js
@@ -36,10 +36,11 @@ function showSlides() {
   let slides = document.getElementsByClassName("mainBanner");
   for (let i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
+    
   }
   slideIndex++;
   if (slideIndex > slides.length) {
-    slideIndex = 2;
+    slideIndex = 1;
   }
 
   slides[slideIndex-1].style.display = "block";
