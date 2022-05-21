50901dd1794637eec0967dce5ac33b6c4c907e1c
marioavaj <predaj@predaj-tonerov.sk>
2022-05-21 14:10:18 +0200

    nahodne produkty na hlavnej strane

--- importProduct.js
+++ importProduct.js
@@ -1,15 +1,33 @@
-let http = new XMLHttpRequest();
-http.open("get", "./categories.json", true);
-http.send();
-
-http.onload = function () {
-  if (this.readyState == 4 && this.status == 200) {
-    console.log(this.responseText)
-    categoryTree = JSON.parse(this.responseText);
-    let output = ``;
-    for (let item of categoryTree) {
-      output += `<a href="#" class="sideMenu">${item.CATEGORY_NAME}</a><br>`;
+fetch("./categories.json")
+  .then((response) => response.json())
+  .then((json) => {
+    for (let i = 0; i < json.length; i++) {
+      let tree = document.getElementById("tree");
+      tree.innerHTML +=
+        `<a href="#" class="sideMenu">` + json[i].CATEGORY_NAME + `</a><br>`;
     }
-    document.querySelector("tree").innerHTML = output;
-  }
-};
+  });
+
+fetch("./products.json")
+  .then((response) => response.json())
+  .then((json) => {
+    for (let i = 0; i < 10; i++) {
+      console.log("som v loope produkty");
+      let item = document.getElementById("wrapper");
+      let randomItem = Math.floor(Math.random() * (json.length-1));
+
+      item.innerHTML +=
+       
+        `<div class="items">`   +     `<h1>` +
+        json[randomItem].PRODUCT_NAME +
+        `</h1>` +
+        `<img src="` +
+        json[randomItem].IMAGE_SRC +
+        `" alt="" />` +
+        `<div class="text">` +
+        json[randomItem].DESCRIPTION +
+        `<br>`+`<br>`+`<span>` +
+        json[randomItem].PRICE_WITH_VAT +
+        `</span><br>`+`<img src="img/basketbutton.png" style="width: 3.5em; margin: auto" alt=""></div>`;
+    }
+  });
