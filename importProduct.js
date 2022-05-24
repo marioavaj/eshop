17f1765e56cf5985409c797a05b65edebc18341c
marioavaj <predaj@predaj-tonerov.sk>
2022-05-24 14:49:03 +0200

    detail produktu

--- importProduct.js
+++ importProduct.js
@@ -12,26 +12,47 @@ fetch("./products.json")
   .then((response) => response.json())
   .then((json) => {
     for (let i = 0; i < 15; i++) {
-      console.log("som v loope produkty");
       let item = document.getElementById("wrapper");
-      let randomItem = Math.floor(Math.random() * (json.length-1));
+      let randomItem = Math.floor(Math.random() * (json.length - 1));
 
       item.innerHTML +=
-       
-        `<div class="items">`   +     
+        `<div class="items">` +
         `<h1>` +
         json[randomItem].PRODUCT_NAME +
         `</h1>` +
-        `<img src="` +
+        `<img class="imgProduct" src="` +
         json[randomItem].IMAGE_SRC +
         `" alt="" />` +
         `<div class="description">` +
-        json[randomItem].DESCRIPTION +`</div>`+
-        `<br>`+
-        `<p id="availability">`+json[randomItem].AVAILABILITY+`</p>`+
-        `<br>`+
+        json[randomItem].DESCRIPTION +
+        `</div>` +
+        `<br>` +
+        `<p id="availability">` +
+        json[randomItem].AVAILABILITY +
+        `</p>` +
+        `<br>` +
         `<span id="price">` +
-        json[randomItem].PRICE_WITH_VAT+`<span id="dph"> €</span></span>`  
-         +`<br>`+`<img src="img/basketbutton.png" style="width: 3.5em; margin: auto" alt=""></div>`;
+        json[randomItem].PRICE_WITH_VAT +
+        `<span id="dph"> €</span></span>` +
+        `<br>` +
+        `<button id="productDetail" onclick="productDetail(`+ randomItem +`)">Detail</button>`+
+        `<br>`+
+        `<br>`;
+        
     }
   });
+
+  function productDetail(randomItem){
+    //let product = window.location.href = "product.html"
+    //let productInfo = product.getElementById("productWrapper");
+
+    fetch("./products.json")
+   .then((response) => response.json())
+   .then((json) => console.log(json[randomItem].PRODUCT_NAME)
+     
+   
+    
+    
+    )
+   }
+  
