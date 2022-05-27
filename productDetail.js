f122f22f6e9736c97a422486bc6596dfcdd32b5f
marioavaj <predaj@predaj-tonerov.sk>
2022-05-27 15:36:00 +0200

    product related

--- productDetail.js
+++ productDetail.js
@@ -11,10 +11,11 @@
       for (let i = 0; i < json.length; i++) {
               
         if(json[i]["ID"] == id){
-            console.log(id);
-            console.log(json[i]["ID"]);
+            
 
             let productDetailWrapper = document.getElementById("productDetailWrapper");
+            var CATEGORY_ID = json[i]["CATEGORY_ID"];
+            
 
            productDetailWrapper.innerHTML=
 
@@ -25,11 +26,39 @@
           `<div id="PRICE"><span class = "productdetailSpan">Cena bez DPH</span><br>${json[i]["PRICE"]}</div>`+
           `<div id="PRICE_WITH_VAT"><span class = "productdetailSpan">Cena s DPH</span><br>${json[i]["PRICE_WITH_VAT"]}</div><br>`+
           `<div id="DESCRIPTION"><span class = "productdetailSpan">Popis:</span><br> ${json[i]["DESCRIPTION"]}</div>`+
-          `<button id="BASKET">Vlož do košíka</button>`;
+          `<button id="BASKET">Vlož do košíka</button>`;  }
+
           
           
+          if(json[i]["CATEGORY_ID"] == CATEGORY_ID){
+            console.log(json[i]["CATEGORY_ID"]);
+            console.log(CATEGORY_ID);
+            let relatedProduktWrapper = document.getElementById('relatedProduktWrapper')
+
+        relatedProduktWrapper.innerHTML +=
+        `<div class="items">` +
+        `<h1 class = "productName">` +
+        json[i].PRODUCT_NAME +
+        `</h1>` +
+        `<img class="imgProduct" src="` +
+        json[i].IMAGE_SRC +
+        `" alt="" />` +
+        `<div class="description">` +
+        json[i].DESCRIPTION +
+        `</div>` +
+        `<br>` +
+        `<p id="availability">` +
+        json[i].AVAILABILITY +
+        `</p>` +
+        `<span id="price">` +
+        json[i].PRICE_WITH_VAT +
+        `<span id="dph"> €</span></span>` +
+        `<a id="productDetail" href= "product.html?id=${json[i].ID}">Detail</a>`;}
+  
 
 
-        }
+          
+
     }
-})
+});
+
