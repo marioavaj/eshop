c5cc8d4c1984db6642cac1a10d29a63428e90a04
marioavaj <predaj@predaj-tonerov.sk>
2022-06-24 15:22:33 +0200

    basket form respons design

--- responsiveMenu.js
+++ responsiveMenu.js
@@ -2,11 +2,11 @@ var displayed = false;
 var menu = document.getElementById("rightExpandMenu");
 
 function expandMenu(){
-
+console.log(displayed);
     if(displayed!=true){
 
 menu.style.display = "flex";
-menu.style.
+
 displayed=true;
     } else if(displayed){
         menu.style.display = "none";
