3e6d2694e7f002bbb26a5ea34d21d07f7489985b
marioavaj <predaj@predaj-tonerov.sk>
2022-06-25 15:44:31 +0200

    searching fixed

--- responsiveMenu.js
+++ responsiveMenu.js
@@ -2,14 +2,17 @@ var displayed = false;
 var menu = document.getElementById("rightExpandMenu");
 
 function expandMenu(){
-console.log(displayed);
+
     if(displayed!=true){
+        
 
 menu.style.display = "flex";
-
+menu.style.transition = "all .5s ease-in-out;" 
+ 
 displayed=true;
     } else if(displayed){
         menu.style.display = "none";
+        menu.style.transition = "all .5s ease-in-out;" 
         displayed=false;
     } 
 }
--- searching.js
+++ searching.js
@@ -1,12 +1,7 @@
 function searching() {
   /**nacitanie retazca od uzivatela */
-  var whatSearching = document.getElementById("search").value;
-  
-  if(whatSearching = " "){
-    whatSearching = document.getElementById("inputSearchMobil").value;
+  var whatSearching = document.getElementById("search").value ;
 
-  }
-  
   whatSearching = whatSearching.toLowerCase();
 
   /**vyhladanie v produktoch v JSON */
@@ -51,76 +46,36 @@ function searching() {
         }
       }
     });
+    
 }
 
-function categoryList(catName) {
-  const idArray = [];
+function searchingMobil() {
+  /**nacitanie retazca od uzivatela */
+   whatSearching = document.getElementById("inputSearchMobil").value;
+    
+  
 
-  fetch("./categories.json")
-    .then((response) => response.json())
-    .then((json) => {
-      for (let i = 0; i < json.length; i++) {
+  whatSearching = whatSearching.toLowerCase();
 
-        let whereSearch = json[i].CATEGORY_PATH;
-         
-        let whatFinded = whereSearch.match(catName);
-        if (whatFinded !=null){
-          idArray.push(json[i]["-ID"])
-        }
-        
+  /**vyhladanie v produktoch v JSON */
 
-        if (json[i].hasOwnProperty("CATEGORIES")) {
-          for (let j = 0; j < json[i].CATEGORIES.CATEGORY.length; j++) {
-             whereSearch = json[i].CATEGORIES.CATEGORY[j].CATEGORY_PATH;
-             
-             whatFinded = whereSearch.match(catName);
-        if (whatFinded !=null){
-          idArray.push(json[i].CATEGORIES.CATEGORY[j]["-ID"])
-          
-        }
-
-
-            if (json[i].CATEGORIES.CATEGORY[j].hasOwnProperty("CATEGORIES")) {
-              for (let k = 0;k < json[i].CATEGORIES.CATEGORY[j].CATEGORIES.CATEGORY.length;  k++){
-
-                whereSearch = json[i].CATEGORIES.CATEGORY[j].CATEGORIES.CATEGORY[k].CATEGORY_PATH;
-                
-                whatFinded = whereSearch.match(catName);
-           if (whatFinded !=null){
-             idArray.push(json[i].CATEGORIES.CATEGORY[j].CATEGORIES.CATEGORY[k]["-ID"])
-             
-
-              }
-              
-            }
-          }
-          
-        }
-      }}
-    })
-
-
-
-    fetch("./products.json")
+  fetch("./products.json")
     .then((response) => response.json())
     .then((json) => {
       let count = 0;
-      
-      const newIdArray = " " + idArray.toString().replaceAll(",", " ");
-      console.log("V tomto stringu " + newIdArray);
+      let whatFinded;
       let searchResults = document.querySelector(".productsWrapper");
       searchResults.innerHTML = ` `;
       for (let i = 0; i < json.length; i++) {
-        let whatIsSearch = json[i]["CATEGORY_ID"];
-       whatIsSearch = whatIsSearch.toString();
-      whatIsFinded = newIdArray.search("\\b" + whatIsSearch + "\\b");
-      
-      if (whatIsFinded !=-1){
-                
-        
+        let whereSearch = json[i].PRODUCT_NAME;
+        whereSearch = whereSearch.toLowerCase();
+
+        whatFinded = whereSearch.match(whatSearching);
+
+        if (whatFinded != null) {
           if (count < 50) {
             count++;
-            
+
             searchResults.innerHTML +=
               `<div class="items">` +
               `<h1 class = "productName">` +
@@ -141,13 +96,101 @@ function categoryList(catName) {
               `<span id="dph"> €</span></span>` +
               `<a id="productDetail" href= "product.html?id=${json[i].ID}">Detail</a>`;
           }
-        } 
-      
-    }
-    }); 
-  
-  };
-
+        }
+      }
+    });
     
+}
 
 
+
+
+
+function categoryList(catName) {
+  const idArray = [];
+
+  fetch("./categories.json")
+    .then((response) => response.json())
+    .then((json) => {
+      for (let i = 0; i < json.length; i++) {
+        let whereSearch = json[i].CATEGORY_PATH;
+
+        let whatFinded = whereSearch.match(catName);
+        if (whatFinded != null) {
+          idArray.push(json[i]["-ID"]);
+        }
+
+        if (json[i].hasOwnProperty("CATEGORIES")) {
+          for (let j = 0; j < json[i].CATEGORIES.CATEGORY.length; j++) {
+            whereSearch = json[i].CATEGORIES.CATEGORY[j].CATEGORY_PATH;
+
+            whatFinded = whereSearch.match(catName);
+            if (whatFinded != null) {
+              idArray.push(json[i].CATEGORIES.CATEGORY[j]["-ID"]);
+            }
+
+            if (json[i].CATEGORIES.CATEGORY[j].hasOwnProperty("CATEGORIES")) {
+              for (
+                let k = 0;
+                k < json[i].CATEGORIES.CATEGORY[j].CATEGORIES.CATEGORY.length;
+                k++
+              ) {
+                whereSearch =
+                  json[i].CATEGORIES.CATEGORY[j].CATEGORIES.CATEGORY[k]
+                    .CATEGORY_PATH;
+
+                whatFinded = whereSearch.match(catName);
+                if (whatFinded != null) {
+                  idArray.push(
+                    json[i].CATEGORIES.CATEGORY[j].CATEGORIES.CATEGORY[k]["-ID"]
+                  );
+                }
+              }
+            }
+          }
+        }
+      }
+    });
+
+  fetch("./products.json")
+    .then((response) => response.json())
+    .then((json) => {
+      let count = 0;
+
+      const newIdArray = " " + idArray.toString().replaceAll(",", " ");
+      console.log("V tomto stringu " + newIdArray);
+      let searchResults = document.querySelector(".productsWrapper");
+      searchResults.innerHTML = ` `;
+      for (let i = 0; i < json.length; i++) {
+        let whatIsSearch = json[i]["CATEGORY_ID"];
+        whatIsSearch = whatIsSearch.toString();
+        whatIsFinded = newIdArray.search("\\b" + whatIsSearch + "\\b");
+
+        if (whatIsFinded != -1) {
+          if (count < 50) {
+            count++;
+
+            searchResults.innerHTML +=
+              `<div class="items">` +
+              `<h1 class = "productName">` +
+              json[i].PRODUCT_NAME +
+              `</h1>` +
+              `<img class="imgProduct" src="` +
+              json[i].IMAGE_SRC +
+              `" alt="" />` +
+              `<div class="description">` +
+              json[i].DESCRIPTION +
+              `</div>` +
+              `<br>` +
+              `<p id="availability">` +
+              json[i].AVAILABILITY +
+              `</p>` +
+              `<span id="price">` +
+              json[i].PRICE_WITH_VAT +
+              `<span id="dph"> €</span></span>` +
+              `<a id="productDetail" href= "product.html?id=${json[i].ID}">Detail</a>`;
+          }
+        }
+      }
+    });
+}
